d74c1f528eb1adac009f730062efa2587fd7dddc
Pinkybanchode <43741842+Pinkybanchode@users.noreply.github.com>
2026-03-19 20:54:58 +0530

    Use environment variable for server port

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
         stack: err.stack
     });
 });
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>{
     console.log(`Server is up and rruning on http://localhost:${PORT}`)
 })
